978057529878d3e6eb56aeeed449d0a078873df9
Navaneeth CT <navaneethct1199@gmail.com>
2024-02-29 19:11:52 +0530

    Remove reservations table footer

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -109,20 +109,6 @@ export default function DashboardPage() {
               </tr>
             ))}
           </tbody>
-
-          <tfoot>
-            {table.getFooterGroups().map((footerGroup) => (
-              <tr key={footerGroup.id}>
-                {footerGroup.headers.map((header) => (
-                  <th key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(header.column.columnDef.footer, header.getContext())}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </tfoot>
         </table>
       </div>
     </div>
